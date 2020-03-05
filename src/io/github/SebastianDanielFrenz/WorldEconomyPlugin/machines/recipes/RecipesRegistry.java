5880e3c1baea403f78990b52bd83b18070014eea
CrashGamesCrMc <crash.games.cr.mc.yt@gmail.com>
2020-03-05 14:36:20 +0100

    stuff

--- src/io/github/SebastianDanielFrenz/WorldEconomyPlugin/machines/recipes/RecipesRegistry.java
+++ src/io/github/SebastianDanielFrenz/WorldEconomyPlugin/machines/recipes/RecipesRegistry.java
@@ -5,60 +5,58 @@ import io.github.SebastianDanielFrenz.WorldEconomyPlugin.machines.items.CustomIt
 import io.github.SebastianDanielFrenz.WorldEconomyPlugin.machines.items.CustomItems;
 
 public class RecipesRegistry {
-	
-	//made by afk worker
-	
-	//Coal proccesing
+
+	// made by afk worker
+
+	// Coal proccesing
 	public static MachineRecipe CRUSHING____COAL_ORE____COAL__COBBLESTONE = new MachineRecipe(
-			new CustomItemStack[] { new CustomItemStack(CustomItems.COAL_ORE, 1) },
-			new CustomItemStack[] { new CustomItemStack(CustomItems.COAL, 1), new CustomItemStack(CustomItems.COBBLESTONE, 1) }, 20);
-	
-	
-	//Iron proccesing
+			new CustomItemStack[] { new CustomItemStack(CustomItems.COAL_ORE, 1) }, new CustomItemStack[] {
+					new CustomItemStack(CustomItems.COAL, 1), new CustomItemStack(CustomItems.COBBLESTONE, 1) },
+			20);
+
+	// Iron proccesing
 	public static MachineRecipe SMELTING____IRON_ORE____IRON_INGOT = new MachineRecipe(
 			new CustomItemStack[] { new CustomItemStack(CustomItems.IRON_ORE, 1) },
 			new CustomItemStack[] { new CustomItemStack(CustomItems.IRON_INGOT, 1) }, 30);
-	
+
 	public static MachineRecipe FORGING____IRON_INGOT____IRON_PLATE = new MachineRecipe(
 			new CustomItemStack[] { new CustomItemStack(CustomItems.IRON_INGOT, 4) },
 			new CustomItemStack[] { new CustomItemStack(CustomItems.IRON_PLATE, 1) }, 30);
-	
+
 	public static MachineRecipe FORGING____IRON_INGOT____IRON_ROD = new MachineRecipe(
 			new CustomItemStack[] { new CustomItemStack(CustomItems.IRON_INGOT, 2) },
 			new CustomItemStack[] { new CustomItemStack(CustomItems.IRON_ROD, 1) }, 30);
-	
+
 	public static MachineRecipe FORGING____IRON_PLATE__IRON_ROD____IRON_PICKAXE = new MachineRecipe(
-			new CustomItemStack[] { new CustomItemStack(CustomItems.IRON_ROD, 4), new CustomItemStack(CustomItems.IRON_PLATE, 1) },
+			new CustomItemStack[] { new CustomItemStack(CustomItems.IRON_ROD, 4),
+					new CustomItemStack(CustomItems.IRON_PLATE, 1) },
 			new CustomItemStack[] { new CustomItemStack(CustomItems.IRON_PICKAXE, 1) }, 600);
-	
+
 	public static MachineRecipe FORGING____IRON_PLATE____IRON_HELMT = new MachineRecipe(
 			new CustomItemStack[] { new CustomItemStack(CustomItems.IRON_PLATE, 2) },
 			new CustomItemStack[] { new CustomItemStack(CustomItems.IRON_HELMET, 1) }, 600);
-	
-	
-	
-	
-	//Gold proccesing
+
+	// Gold proccesing
 	public static MachineRecipe SMELTING____GOLD_ORE____GOLD_NUGGET = new MachineRecipe(
 			new CustomItemStack[] { new CustomItemStack(CustomItems.GOLD_ORE, 1) },
 			new CustomItemStack[] { new CustomItemStack(CustomItems.GOLD_NUGGET, 1) }, 60);
-	
-	public static MachineRecipe SMELTING___GOLD_NUGGET___GOLD_INGOT = new MachineRecipe(
+
+	public static MachineRecipe SMELTING____GOLD_NUGGET____GOLD_INGOT = new MachineRecipe(
 			new CustomItemStack[] { new CustomItemStack(CustomItems.GOLD_NUGGET, 10) },
 			new CustomItemStack[] { new CustomItemStack(CustomItems.GOLD_INGOT, 1) }, 30);
-	
-	public static MachineRecipe FORGING___GOLD_INGOT___GOLD_ROD = new MachineRecipe(
+
+	public static MachineRecipe FORGING____GOLD_INGOT____GOLD_ROD = new MachineRecipe(
 			new CustomItemStack[] { new CustomItemStack(CustomItems.GOLD_INGOT, 2) },
 			new CustomItemStack[] { new CustomItemStack(CustomItems.GOLD_ROD, 1) }, 30);
-	
-	public static MachineRecipe CRAFTING___GOLD_ROD___GOLD_CABLE = new MachineRecipe(
+
+	public static MachineRecipe CRAFTING____GOLD_ROD____GOLD_CABLE = new MachineRecipe(
 			new CustomItemStack[] { new CustomItemStack(CustomItems.GOLD_ROD, 1) },
 			new CustomItemStack[] { new CustomItemStack(CustomItems.GOLD_CABLE, 10) }, 30);
-	
-	//dirt processing
+
+	// dirt processing
 	public static MachineRecipe SIEVING____COARSE_DIRT____COBBLESTONE__DIRT = new MachineRecipe(
 			new CustomItemStack[] { new CustomItemStack(CustomItems.COARSE_DIRT, 16) }, new CustomItemStack[] {
 					new CustomItemStack(CustomItems.COBBLESTONE, 1), new CustomItemStack(CustomItems.DIRT, 16) },
 			12);
-	
+
 }
--- src/io/github/SebastianDanielFrenz/WorldEconomyPlugin/machines/research/ResearchItem.java
+++ src/io/github/SebastianDanielFrenz/WorldEconomyPlugin/machines/research/ResearchItem.java
@@ -1,5 +1,11 @@
 package io.github.SebastianDanielFrenz.WorldEconomyPlugin.machines.research;
 
+import io.github.SebastianDanielFrenz.WorldEconomyPlugin.machines.Machine;
+
 public abstract class ResearchItem {
+
+	public abstract ResearchItem[] getParents();
+
+	public abstract Machine getMachine();
 
 }
