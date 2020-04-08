53ac84074e0d01bc6dac2f9675cd918700a7951f
Sebastian Frenz <sebastian.daniel.frenz@gmail.com>
2020-04-08 23:47:02 +0200

    Update ItemSandstoneTrigger.java

--- src/io/github/SebastianDanielFrenz/WorldEconomyPlugin/gameplay/item/items/ItemSandstoneTrigger.java
+++ src/io/github/SebastianDanielFrenz/WorldEconomyPlugin/gameplay/item/items/ItemSandstoneTrigger.java
@@ -2,11 +2,11 @@ package io.github.SebastianDanielFrenz.WorldEconomyPlugin.gameplay.item.items;
 
 import org.bukkit.Material;
 
-import io.github.SebastianDanielFrenz.WorldEconomyPlugin.gameplay.item.CustomItem;
+import io.github.SebastianDanielFrenz.WorldEconomyPlugin.gameplay.item.CustomPlaceableItem;
 import io.github.SebastianDanielFrenz.WorldEconomyPlugin.gameplay.item.ItemCategory;
 import io.github.SebastianDanielFrenz.WorldEconomyPlugin.gameplay.item.Tier;
 
-public class ItemSandstoneTrigger extends CustomItem{
+public class ItemSandstoneTrigger extends CustomPlaceableItem {
 
 	public ItemSandstoneTrigger() {
 		super("sandstone_trigger", Material.CHISELED_SANDSTONE, Tier.TIER1, "Sandstone Button", ItemCategory.BUILDING);
