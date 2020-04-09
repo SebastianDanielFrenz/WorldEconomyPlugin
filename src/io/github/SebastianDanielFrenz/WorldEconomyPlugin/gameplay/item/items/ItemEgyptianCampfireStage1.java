2fc96048839382504240be787c8884bbbe8a37f9
Sebastian Frenz <sebastian.daniel.frenz@gmail.com>
2020-04-09 16:17:18 +0200

    fix

--- src/io/github/SebastianDanielFrenz/WorldEconomyPlugin/gameplay/item/items/ItemEgyptianCampfireStage1.java
+++ src/io/github/SebastianDanielFrenz/WorldEconomyPlugin/gameplay/item/items/ItemEgyptianCampfireStage1.java
@@ -2,11 +2,11 @@ package io.github.SebastianDanielFrenz.WorldEconomyPlugin.gameplay.item.items;
 
 import org.bukkit.Material;
 
-import io.github.SebastianDanielFrenz.WorldEconomyPlugin.gameplay.item.CustomItem;
+import io.github.SebastianDanielFrenz.WorldEconomyPlugin.gameplay.item.CustomPlaceableItem;
 import io.github.SebastianDanielFrenz.WorldEconomyPlugin.gameplay.item.ItemCategory;
 import io.github.SebastianDanielFrenz.WorldEconomyPlugin.gameplay.item.Tier;
 
-public class ItemEgyptianCampfireStage1 extends CustomItem {
+public class ItemEgyptianCampfireStage1 extends CustomPlaceableItem {
 
 	public ItemEgyptianCampfireStage1() {
 		super("egyptian_campfire_stage1", Material.CAMPFIRE, Tier.TIER1, "Egyptian Campfire Stage 1",
