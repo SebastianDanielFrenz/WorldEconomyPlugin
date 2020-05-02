debc92698e9d9e10b1eff8ea96c6d888a7f2171a
CrashGamesCrMc <crash.games.cr.mc.yt@gmail.com>
2020-05-02 13:34:59 +0200

    Create ResearchItemPlanks.java

---
+++ src/io/github/SebastianDanielFrenz/WorldEconomyPlugin/gameplay/research/researchItems/early_stone_age/ResearchItemPlanks.java
@@ -0,0 +1,20 @@
+package io.github.SebastianDanielFrenz.WorldEconomyPlugin.gameplay.research.researchItems.early_stone_age;
+
+import io.github.SebastianDanielFrenz.WorldEconomyPlugin.gameplay.Age;
+import io.github.SebastianDanielFrenz.WorldEconomyPlugin.gameplay.block.CustomBlockTypeRegistry;
+import io.github.SebastianDanielFrenz.WorldEconomyPlugin.gameplay.item.CustomItemRegistry;
+import io.github.SebastianDanielFrenz.WorldEconomyPlugin.gameplay.research.ExperienceResearchCondition;
+import io.github.SebastianDanielFrenz.WorldEconomyPlugin.gameplay.research.ResearchCondition;
+import io.github.SebastianDanielFrenz.WorldEconomyPlugin.gameplay.research.ResearchItem;
+import io.github.SebastianDanielFrenz.WorldEconomyPlugin.gameplay.research.StatisticCategoryRegistry;
+
+public class ResearchItemPlanks extends ResearchItem {
+
+	public ResearchItemPlanks() {
+		super("Stick", CustomItemRegistry.STICK, new ResearchItem[] {},
+				new ResearchCondition[] {
+						new ExperienceResearchCondition(CustomBlockTypeRegistry.TREE_OAK_LEAVES, StatisticCategoryRegistry.MINED, 10) },
+				Age.EARLY_STONE_AGE);
+	}
+
+}
