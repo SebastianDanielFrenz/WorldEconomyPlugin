68fbf1b9fdaa29547231f87f82a09b1057f8fa58
Sebastian Frenz <sebastian.daniel.frenz@gmail.com>
2020-05-20 00:14:51 +0200

    sql task part 1

---
+++ src/io/github/SebastianDanielFrenz/WorldEconomyPlugin/multithreading/tasking/tasks/SQLTask.java
@@ -0,0 +1,58 @@
+package io.github.SebastianDanielFrenz.WorldEconomyPlugin.multithreading.tasking.tasks;
+
+import java.sql.SQLException;
+
+import io.github.SebastianDanielFrenz.WorldEconomyPlugin.WorldEconomyPlugin;
+import io.github.SebastianDanielFrenz.WorldEconomyPlugin.multithreading.tasking.Task;
+
+public class SQLTask extends Task {
+	
+	private String query;
+	
+	public SQLTask(String query) {
+		this.query = query;
+	}
+
+	@Override
+	public int getPriority() {
+		return -1000;
+	}
+
+	@Override
+	public void init() {
+	}
+
+	@Override
+	public void work() {
+		try {
+			WorldEconomyPlugin.runSQL(query);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public void discard() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public boolean startOnShutdown() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean continueOnShutdown() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean hasFinished() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
