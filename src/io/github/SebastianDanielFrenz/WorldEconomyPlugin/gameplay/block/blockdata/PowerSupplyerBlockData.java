fcfc79cb1b1199bf8b38f1e83064e3cc74b84881
CrashGamesCrMc <crash.games.cr.mc.yt@gmail.com>
2020-08-29 12:25:46 +0200

    Create PowerSupplyerBlockData.java

---
+++ src/io/github/SebastianDanielFrenz/WorldEconomyPlugin/gameplay/block/blockdata/PowerSupplyerBlockData.java
@@ -0,0 +1,27 @@
+package io.github.SebastianDanielFrenz.WorldEconomyPlugin.gameplay.block.blockdata;
+
+import org.bukkit.Location;
+
+import io.github.SebastianDanielFrenz.WorldEconomyPlugin.gameplay.block.machine.electric.PowerGridMemberType;
+
+public class PowerSupplyerBlockData extends PowerConnectedBlockData {
+
+	public PowerSupplyerBlockData(Location location) {
+		super(location);
+	}
+
+	public PowerSupplyerBlockData(Location location, String raw) {
+		super(location, raw);
+	}
+
+	@Override
+	public PowerGridMemberType getPowerGridMemberType() {
+		return 
+	}
+
+	@Override
+	public String save() {
+		return null;
+	}
+
+}
