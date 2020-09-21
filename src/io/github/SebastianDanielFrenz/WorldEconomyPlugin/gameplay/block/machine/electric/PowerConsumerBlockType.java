07529e3ed4896ef623ed0bce15b620e365836b00
CrashGamesCrMc <crash.games.cr.mc.yt@gmail.com>
2020-09-21 08:42:09 +0200

    power grid stuff

--- src/io/github/SebastianDanielFrenz/WorldEconomyPlugin/gameplay/block/machine/electric/PowerConsumerBlockType.java
+++ src/io/github/SebastianDanielFrenz/WorldEconomyPlugin/gameplay/block/machine/electric/PowerConsumerBlockType.java
@@ -15,10 +15,47 @@ public abstract class PowerConsumerBlockType extends CustomBlockType {
 		super(plugin, ID, material, data, vanilla, drop_table, blockDataType);
 	}
 
+	/**
+	 * Returns the max amount of power that can be accepted by the block in this
+	 * tick.
+	 * 
+	 * @param location
+	 * @param blockData
+	 * @return
+	 */
 	public abstract double getMaxPower(Location location, CustomBlockData blockData);
 
+	/**
+	 * Returns true, if the given amount of power can be used by the
+	 * machine.<br>
+	 * <br>
+	 * This can be used to enforce a min power.
+	 * 
+	 * @param location
+	 * @param blockData
+	 * @param amount
+	 * @return
+	 */
 	public abstract boolean acceptPower(Location location, CustomBlockData blockData, double amount);
 
-	public abstract void usePower(Location location, CustomBlockData blockData, double amount);
+	/**
+	 * Saves the amount of power for later processing after finishing the power
+	 * distribution. This method can be called multiple times in one tick.
+	 * 
+	 * @param location
+	 * @param blockData
+	 * @param amount
+	 */
+	public abstract void assignPower(Location location, CustomBlockData blockData, double amount);
+
+	/**
+	 * Uses the assigned power. This should enclude the machine's working
+	 * process and resetting the temporary power storage ussed to track the
+	 * assigned power.
+	 * 
+	 * @param location
+	 * @param blockData
+	 */
+	public abstract void usePower(Location location, CustomBlockData blockData);
 
 }
--- src/io/github/SebastianDanielFrenz/WorldEconomyPlugin/gameplay/block/machine/electric/PowerGrid.java
+++ src/io/github/SebastianDanielFrenz/WorldEconomyPlugin/gameplay/block/machine/electric/PowerGrid.java
@@ -83,66 +83,17 @@ public class PowerGrid {
 	}
 
 	public void distributePower() {
-		double total_possible_consumption = 0;
-
+		
+		
 		for (Block consumer : consumers) {
 			CustomBlockMetadataValue meta = CustomBlockTypeRegistry.getBlockDetails(consumer);
-			total_possible_consumption += ((PowerConsumerBlockType) meta.getBlock()).getMaxPower(consumer.getLocation(), meta.getBlockData());
+			((PowerConsumerBlockType) meta.getBlock()).assignPower(consumer.getLocation(), meta.getBlockData(),
+					((PowerConsumerBlockType) meta.getBlock()).getMaxPower(consumer.getLocation(), meta.getBlockData()));
 		}
-
-		double total_possible_supply = 0;
-
-		for (Block supplyer : supplyers) {
-			CustomBlockMetadataValue meta = CustomBlockTypeRegistry.getBlockDetails(supplyer);
-			total_possible_supply += ((PowerSupplyerBlockType) meta.getBlock()).getPowerOutput(supplyer.getLocation(), meta.getBlockData(),
-					total_possible_consumption - total_possible_supply);
-		}
-
-		if (total_possible_consumption > total_possible_supply) {
-			double total_possible_storage_out = 0;
-
-			for (Block storage : storages) {
-				CustomBlockMetadataValue meta = CustomBlockTypeRegistry.getBlockDetails(storage);
-				total_possible_storage_out += ((PowerStorageBlockType) meta.getBlock()).getMaxPowerOutput(storage.getLocation(), meta.getBlockData());
-			}
-
-			if (total_possible_consumption > total_possible_supply + total_possible_storage_out) {
-				for (Block consumer : consumers) {
-					CustomBlockMetadataValue consumerMeta = CustomBlockTypeRegistry.getBlockDetails(consumer);
-					PowerConsumerBlockType blockType = ((PowerConsumerBlockType) consumerMeta.getBlock());
-
-					double max = blockType.getMaxPower(consumer.getLocation(), consumerMeta.getBlockData());
-
-					if (max > total_possible_supply + total_possible_storage_out) {
-						if (blockType.acceptPower(consumer.getLocation(), consumerMeta.getBlockData(),
-								total_possible_storage_out + total_possible_supply)) {
-							for (Block supplyer : supplyers) {
-								CustomBlockMetadataValue supplyerMeta = CustomBlockTypeRegistry.getBlockDetails(supplyer);
-								double max_supplyer_output = ((PowerSupplyerBlockType) consumerMeta.getBlock()).getPowerOutput(supplyer.getLocation(),
-										consumerMeta.getBlockData(), max);
-								((PowerSupplyerBlockType) supplyerMeta.getBlock()).usePower(supplyer.getLocation(), supplyerMeta.getBlockData(),
-										max_supplyer_output);
-								// if getPowerOutput makes ingame changes, this
-								// needs to be run
-							}
-							for (Block storage : storages) {
-								CustomBlockMetadataValue storageMeta = CustomBlockTypeRegistry.getBlockDetails(storage);
-								double max_storage_output = ((PowerStorageBlockType) storageMeta.getBlock()).getMaxPowerOutput(storage.getLocation(),
-										storageMeta.getBlockData());
-								((PowerStorageBlockType) storageMeta.getBlock()).usePower(storage.getLocation(), storageMeta.getBlockData(),
-										max_storage_output);
-							}
-							return; // all the power is used up
-						} else {
-							continue;
-						}
-					} else {
-						if (max > total_possible_supply) {
-
-						}
-					}
-				}
-			}
+		
+		for (Block consumer:consumers) {
+			CustomBlockMetadataValue meta = CustomBlockTypeRegistry.getBlockDetails(consumer);
+			((PowerConsumerBlockType) meta.getBlock()).usePower(consumer.getLocation(), meta.getBlockData());
 		}
 	}
 
